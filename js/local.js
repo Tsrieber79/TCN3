dee6b9f7d23ed411b65fe921e73b388df3222a86
Sago500 <tsrieber79@gmail.com>
2018-07-26 19:21:04 -0500

    fifth commit

--- js/local.js
+++ js/local.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $("#AboutLink").click(function() {
         $.scrollTo("#About", 800, {offset:-70});
         });
+
+    $('#TitleLink').click(function() {
+        $.scrollTo('#Title', 1000,)
+    })
 });
